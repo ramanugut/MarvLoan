c1634e8793cdb30f01198508b76a4d7396b458b9
Thembuluwo Ramanugu <42994454+ramanugut@users.noreply.github.com>
2025-06-22 21:49:21 +0200

    Simplify App routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './features/auth/Login';
-import ForgotPassword from './features/auth/ForgotPassword';
-import UserList from './features/admin/UserList';
-import NoAccess from './components/NoAccess';
-import PrivateRoute from './components/PrivateRoute';
-import { AuthProvider } from './contexts/AuthContext';
 
-const App: React.FC = () => (
-  <AuthProvider>
+function App() {
+  return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route
-          path="/admin/users"
-          element={
-            <PrivateRoute roles={["Admin"]}>
-              <UserList />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/no-access" element={<NoAccess />} />
-        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
-  </AuthProvider>
-);
+  );
+}
 
 export default App;
